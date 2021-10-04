26b0f556a6331ec2ed8d1965eba1a047504d721b
tompvr26 <tompoyvre@gmail.com>
2021-10-04 12:26:15 +0200

    UI menu OK

--- ABCDC/Program.cs
+++ ABCDC/Program.cs
@@ -6,7 +6,35 @@ namespace ABCDC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("            _ __ ___   ___ _ __  _   _ ");
+            Console.WriteLine("           | '_ ` _ \\ / _ \\ '_ \\| | | |");
+            Console.WriteLine("           | | | | | |  __/ | | | |_| |");
+            Console.WriteLine("           |_| |_| |_|\\___|_| |_|\\__,_|");
+
+            Console.WriteLine("\n\nChoisir une option:\n");
+            Console.WriteLine("    1- Ajouter un nouveau participant");
+            Console.WriteLine("    2- Ajouter un nouvel adéhrant");
+            Console.WriteLine("    3- Créer des équipes pour le prochain match");
+            Console.WriteLine("    4- Quitter");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    //redirection
+                    break;
+
+                case "2":
+                    //redirection
+                    break;
+
+                case "3":
+                    //redirection
+                    break;
+
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
         }
     }
 }
