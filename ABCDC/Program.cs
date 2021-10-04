267c903eb8e64d363d8d638a396e38a114fa2260
tompvr26 <tompoyvre@gmail.com>
2021-10-04 16:21:37 +0200

    add player OK

--- ABCDC/Program.cs
+++ ABCDC/Program.cs
@@ -6,60 +6,118 @@ namespace ABCDC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("            _ __ ___   ___ _ __  _   _ ");
-            Console.WriteLine("           | '_ ` _ \\ / _ \\ '_ \\| | | |");
-            Console.WriteLine("           | | | | | |  __/ | | | |_| |");
-            Console.WriteLine("           |_| |_| |_|\\___|_| |_|\\__,_|");
+            string option = "";
 
-            Console.WriteLine("\n\nBienvenu:\n");
-            Console.WriteLine("    1- Ajouter un nouveau participant");
-            Console.WriteLine("    2- Ajouter un nouvel adéhrant");
-            Console.WriteLine("    3- Créer des équipes pour le prochain match");
-            Console.WriteLine("    4- Quitter");
-
-            Console.Write("\nChoisir une option : ");
-            switch (Console.ReadLine())
+            do
             {
-                case "1":
-                    //redirection
-                    Console.Clear();
+                Console.Clear();
 
-                    Console.WriteLine("\n\n     Pour l'ajout d'un nouveau participant,");
-                    Console.WriteLine("     veuillez saisir les informations suivantes.");
+                Console.WriteLine("            _ __ ___   ___ _ __  _   _ ");
+                Console.WriteLine("           | '_ ` _ \\ / _ \\ '_ \\| | | |");
+                Console.WriteLine("           | | | | | |  __/ | | | |_| |");
+                Console.WriteLine("           |_| |_| |_|\\___|_| |_|\\__,_|");
 
-                    Console.Write("\n     Nom : ");
-                    Console.ReadLine();
+                Console.WriteLine("\n\nBienvenu:\n");
+                Console.WriteLine("    1- Gestion des participants");
+                Console.WriteLine("    2- Gestion des adéhrants");
+                Console.WriteLine("    3- Créer des équipes pour le prochain match");
+                Console.WriteLine("    4- Quitter");
 
-                    Console.Write("     Prenom : ");
-                    Console.ReadLine();
+                Console.Write("\nChoisir une option : ");
 
-                    Console.Write("     Ancienneté : ");
-                    Console.ReadLine();
+                option = Console.ReadLine();
 
-                    Console.Write("     Armure : ");
-                    Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        //redirection
+                        Console.Clear();
 
-                    Console.Write("     Arme : ");
-                    Console.ReadLine();
+                        Console.WriteLine("\n\n     1- Ajouter un participant");
+                        Console.WriteLine("     2- Supprimer un participant");
+                        Console.WriteLine("     3- Voir la liste des participants");
+                        Console.WriteLine("     4- Retour au menu");
 
-                    break;
+                        Console.Write("\nChoisir une option : ");
+                        switch (Console.ReadLine())
+                        {
+                            //Ajouter participants
+                            case "1":
+                                Console.Clear();
 
-                case "2":
-                    //redirection
-                    Console.Clear();
+                                Console.WriteLine("\n\n     Pour l'ajout d'un nouveau participant,");
+                                Console.WriteLine("     veuillez saisir les informations suivantes.");
 
-                    break;
+                                Person p = new Person();
 
-                case "3":
-                    //redirection
-                    Console.Clear();
+                                Console.Write("\n     Nom : ");
+                                p.LastName = Console.ReadLine();
 
-                    break;
+                                Console.Write("     Prenom : ");
+                                p.FirstName = Console.ReadLine();
 
-                default:
-                    Environment.Exit(0);
-                    break;
-            }
+                                Console.Write("     Ancienneté : ");
+                                p.BeginYear = Int32.Parse(Console.ReadLine());
+
+                                Console.Write("     Poids : ");
+                                p.Weight = Int32.Parse(Console.ReadLine());
+
+                                Console.Write("     Armure : ");
+                                p.Armor = Console.ReadLine();
+
+                                Console.Write("     Arme : ");
+                                p.Weapon = Console.ReadLine();
+
+                                Console.Clear();
+                                Console.WriteLine("\n\n     Votre nouveau participant:");
+                                Console.WriteLine("\n     " + p.LastName + " " + p.FirstName);
+                                Console.WriteLine("     " + p.Weight);
+                                Console.WriteLine("     " + p.BeginYear);
+                                Console.WriteLine("     " + p.Armor);
+                                Console.WriteLine("     " + p.Weapon);
+
+                                Console.Write("\n\n\nAppuyer sur <Enter> pour retourner au menu... ");
+                                while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+
+                                break;
+
+                            //Suprimer particpant
+                            case "2":
+
+                                break;
+
+                            //Voir liste particpants
+                            case "3":
+
+                                break;
+
+                            default:
+
+                                break;
+                        }
+
+                        break;
+
+                    case "2":
+                        //redirection
+                        Console.Clear();
+
+                        break;
+
+                    case "3":
+                        //redirection
+                        Console.Clear();
+
+                        break;
+
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+
+            } while (option != "4");
+
+            
         }
     }
 }
