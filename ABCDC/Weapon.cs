6a0352ce1df806fc6745a336b1b3e7472f05e4b5
EzHeisenberg <lucaporal@live.fr>
2021-10-05 08:18:17 +0200

    add weapon class

--- ABCDC/Weapon.cs
+++ ABCDC/Weapon.cs
@@ -3,6 +3,23 @@ namespace ABCDC
 {
     public class Weapon
     {
+
+        #region Properties 
+
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        #endregion
+
+        #region Attributes
+
+        private string _name;
+
+        #endregion
+
         public Weapon()
         {
         }
